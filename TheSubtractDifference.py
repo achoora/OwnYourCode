94aef72ba15666fb77804de0fee2d1801ae10608
achoora <achoora@users.noreply.github.com>
2020-12-02 21:37:08 +0530

    Add files via upload
    simple difference function

---
+++ TheSubtractDifference.py
@@ -0,0 +1,18 @@
+def diff(x):
+  y=4
+  z=-2
+  dif=y-x-z
+  print("The Operation is y -x -z Where X Y Z are ")
+  print(str(y)+'          '+str(x)+'        '+str(z))
+  print("The result is ")
+  print(dif)
+
+diff(2)
+def subtract(x,y):
+  i=y
+  res=0
+  while i>0:
+    res=x-i
+    i=i-1
+  print(res) 
+subtract(8,1)
