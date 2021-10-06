5f928b7539565096b88c396f7a33d7d5c0f880a8
achoora <achoora@users.noreply.github.com>
2021-10-06 14:55:36 +0530

    Testing of Block Scope variables.

---
+++ BlockScopeTest.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+Block Scope variables. 
+The arguements of the function and the local variables inside the function 
+declarations are in block scope so their names cannot be the same 
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    char *nameofsructure; 
+    int id_num; 
+}; 
+
+int newname(struct node * value); 
+
+int main()
+{
+    printf("Hello World\n");
+    struct node *local=(struct node *)malloc(sizeof(struct node));
+    int getaback=newname(local);
+
+    return 0;
+}
+int newname(struct node * value)
+{
+//    int value; 
+//main.c:28:9: error: ‘value’ redeclared as different kind of symbol
+      int Value; 
+    printf("The arguement name and local variable name are not same ");
+    
+}
