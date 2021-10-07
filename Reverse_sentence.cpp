e05632bc96d17399f1d68b19c2b01424a9e87d24
achoora <achoora@users.noreply.github.com>
2021-10-07 08:51:05 +0530

    Reverse a Sentence using pointers and strlen operator.

---
+++ Reverse_sentence.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+//Reverse a Sentence using pointers and strlen operator. 
+
+*******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    
+    char *string="reverse the words in the string";
+    int l_count=0;
+    int length=strlen(string);
+    printf("string length is %d\n",length);
+    char *destination=string+length;
+    int iter=0;       
+    while(*string!='\0')
+    {
+       
+        if(*destination!=' ') //Back ward traversal till the empty space 
+        {
+           ++l_count; //keep the distance from the end of word o  the prefix space 
+           iter=0;
+        }
+        else
+        {
+        while(iter<l_count) 
+       {
+       ++iter;
+       printf("%c",*(destination+iter));
+       }
+       printf("\t");
+       l_count=0;
+       }
+       if(length==1)
+       {
+       --destination;
+       while(iter<=l_count) 
+       {
+
+       printf("%c",*(destination+iter));
+            ++iter;
+       }
+       }
+       string++;
+       destination--; 
+       length--;
+    }
+    return 0;
+}
